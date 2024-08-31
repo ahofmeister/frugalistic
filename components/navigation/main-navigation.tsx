a6e434aef589b71538ef70eb82c9edb6978efa71
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2024-08-31 10:04:41 +0200

    Fix app/auth button

--- components/navigation/main-navigation.tsx
+++ components/navigation/main-navigation.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 
-import AuthButton from "@/components/auth/auth-button";
 import { NavItem } from "@/components/navigation/nav-config";
 
 const MainNavigation = ({
