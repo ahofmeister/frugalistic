65c63355054d37ebd89f4a751adf52865e5a4189
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2024-11-10 11:45:50 +0100

    Change description

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+// app/layout.tsx
+import { Metadata } from "next";
+import React from "react";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "Frugalistic",
+  description:
+    "Take control of your finances · Track expenses, income, and savings · Visualize your financial health · Stay on top of your finances.",
+  keywords: [
+    "frugal",
+    "expense tracker",
+    "income tracker",
+    "savings",
+    "finance management",
+    "transactions",
+    "recurring payments",
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <>{children}</>
+    </>
+  );
+}
