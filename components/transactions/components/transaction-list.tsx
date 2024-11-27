f10682cc3cb5cdc5193ad02de3161a4a02624840
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2024-11-27 18:19:10 +0100

    feat: Add row count to transactions

--- components/transactions/components/transaction-list.tsx
+++ components/transactions/components/transaction-list.tsx
@@ -28,6 +28,9 @@ export default function TransactionList({
 }) {
   return (
     <div className="w-full ">
+      <div className="text-muted-foreground flex justify-end pt-1 pb-2">
+        {transactions.length} transactions
+      </div>
       <Table>
         <TableHeader>
           <TableRow>
