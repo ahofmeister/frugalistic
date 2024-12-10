f0af91bfae03c01173b49245f6a59572424e1de0
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2024-12-10 07:31:52 +0100

    fix Linting errors

--- app/dashboard/recurring/page.tsx
+++ app/dashboard/recurring/page.tsx
@@ -3,7 +3,7 @@ import React, { Suspense } from "react";
 import RecurringTransactionTable from "@/app/dashboard/recurring/recurring-transaction-table";
 import LoadingSpinner from "@/components/loading/loading";
 
-export default async function TransactionsPage() {
+export default function TransactionsPage() {
   return (
     <>
       <div className="flex gap-10">
