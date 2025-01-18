344695addad47b8ea45c32ce8a1f2100b6a83da5
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2025-01-18 09:03:16 +0100

    feat: Add empty state for recurring transactions

--- app/dashboard/recurring/recurring-transactions.tsx
+++ app/dashboard/recurring/recurring-transactions.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
 import React from "react";
 
 import RecurringTransactionCard from "@/app/dashboard/recurring/recurring-transaction-card";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import { createClient } from "@/utils/supabase/server";
 
 const RecurringTransactions = async () => {
@@ -14,6 +17,17 @@ const RecurringTransactions = async () => {
   return (
     <div className="w-full">
       <div className="text-2xl font-semibold mb-4">Recurring Transactions</div>
+      {transactions?.length === 0 && (
+        <Card className=" inset-0 gap-y-4 flex flex-col h-[240px] items-center justify-center  text-muted-foreground text-lg">
+          No recurring transactions to display yet!
+          <Link href="/dashboard/transactions/new">
+            <Button variant="default">
+              <div className="flex">New Transaction</div>
+            </Button>
+          </Link>
+        </Card>
+      )}
+
       <div className="flex flex-col gap-y-2">
         {transactions?.map((transaction) => (
           <RecurringTransactionCard
