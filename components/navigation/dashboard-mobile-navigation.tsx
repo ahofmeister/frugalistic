b848b3f646df3a5a8154cd5975637fc400e0f937
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2025-01-18 20:55:36 +0100

    feat: Use bottom navigation for mobile

--- components/navigation/dashboard-mobile-navigation.tsx
+++ components/navigation/dashboard-mobile-navigation.tsx
@@ -10,14 +10,14 @@ export function DashboardMobileNavigation() {
   const pathname = usePathname();
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 shrink-0 border-t bg-background md:hidden">
-      <div className="flex h-20 items-center justify-between px-4">
+    <div className="fixed bottom-0 left-0 right-0 shrink-0 border-t bg-background md:hidden pb-[calc(0.5rem+env(safe-area-inset-bottom))]">
+      <div className="flex h-20 justify-between items-center px-2">
         {navConfig.dashboardNavigation.map((item) => (
           <Link
             key={item.href}
             href={item.href}
             className={cn(
-              "flex flex-col items-center justify-center w-full",
+              "flex flex-col  w-full gap-1 items-center justify-center",
               pathname === item.href
                 ? "text-primary"
                 : "text-muted-foreground hover:text-primary",
