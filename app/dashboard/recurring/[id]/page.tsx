8935216f79d930dfa310c2abc33cb3d3295fc010
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2025-11-15 19:14:09 +0100

    Upgrade to Next.JS 16

--- app/dashboard/recurring/[id]/page.tsx
+++ app/dashboard/recurring/[id]/page.tsx
@@ -4,15 +4,29 @@ import { RecurringTransactionHistory } from "@/app/dashboard/recurring/[id]/recu
 import { createClient } from "@/utils/supabase/server";
 import RecurringTransactionForm from "@/components/transactions/recurring/components/recurring-transaction-form";
 import { Suspense } from "react";
+import DeleteRecurringTransaction from "@/app/dashboard/recurring/delete-recurring-transaction";
 
 export default async function TransactionEditPage(props: {
   params: Promise<{ id: string }>;
 }) {
   return (
     <div>
-      <Suspense>
-        <RecurringTransactionWrapper id={props.params.then((p) => p.id)} />
-      </Suspense>
+      <div className="space-y-6 mx-auto max-w-lg">
+        <div className="flex flex-col gap-y-4">
+          <Suspense>
+            <RecurringTransactionWrapper id={props.params.then((p) => p.id)} />
+          </Suspense>
+          <div className="my-8 mx-auto w-full">
+            <div className="w-full">
+              <Suspense>
+                <DeleteRecurringTransaction
+                  id={props.params.then((p) => p.id)}
+                />
+              </Suspense>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="mt-6 mx-auto max-w-lg">
         <div className="text-lg mb-2">Transaction History</div>
         <Suspense>
