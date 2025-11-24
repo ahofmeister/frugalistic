7fecd02a782e90b2a23c1dad36402efe47df3e1d
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2025-11-24 19:04:16 +0100

    feat: Add annual overview page (#38)

---
+++ app/(dashboard)/annual/page.tsx
@@ -0,0 +1,14 @@
+import React, { Suspense } from "react";
+import YearComparison from "@/components/year-comparison";
+
+const AnnualPage = () => {
+  return (
+    <div>
+      <Suspense>
+        <YearComparison />
+      </Suspense>
+    </div>
+  );
+};
+
+export default AnnualPage;
